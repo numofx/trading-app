10c58db992bea2a1b941d59ededa8dbe930b4b4b
Robert Leifke <r.leifke@gmail.com>
2026-07-23 18:17:17 +0100

    chore: add PostHog client/server init files (imported by orders route)

---
+++ lib/posthog-server.ts
@@ -0,0 +1,10 @@
+import "server-only";
+import { PostHog } from "posthog-node";
+
+export function getPostHogClient() {
+  return new PostHog(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN ?? "", {
+    host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    flushAt: 1,
+    flushInterval: 0,
+  });
+}
